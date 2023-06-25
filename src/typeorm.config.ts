e3f0aa2cf126b8f6de785a6c07af372cb424ae6a
Moiz Kachwala <moizk@hotmail.com>
2023-06-25 10:15:04 +0400

    env variables

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -3,14 +3,15 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { User } from './user/user.entity';
 import { Role } from './role/role.entity';
 import { Permission } from './permission/permission.entity';
+import 'dotenv/config';
 
 const config: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: '',
-  database: 'nest_chat',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   entities: [User, Role, Permission],
   synchronize: true,
 };
