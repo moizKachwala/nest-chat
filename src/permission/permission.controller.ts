f2e62a78e05a1984dfe24fce71a3576632e417e7
Moiz Kachwala <moizk@hotmail.com>
2023-06-26 18:57:19 +0400

    added permissions

--- src/permission/permission.controller.ts
+++ src/permission/permission.controller.ts
@@ -1,7 +1,34 @@
-// permission.controller.ts
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Delete, Body, Param } from '@nestjs/common';
+import { PermissionService } from './permission.service';
+import { CreatePermissionDto } from './dto/create-permission.dto';
+import { UpdatePermissionDto } from './dto/update-permission.dto';
 
 @Controller('permissions')
 export class PermissionController {
-  // Implement your permission-related routes here
+  constructor(private readonly permissionService: PermissionService) {}
+
+  @Get()
+  findAll() {
+    return this.permissionService.findAll();
+  }
+
+  @Get(':id')
+  findById(@Param('id') id: number) {
+    return this.permissionService.findById(id);
+  }
+
+  @Post()
+  create(@Body() createPermissionDto: CreatePermissionDto) {
+    return this.permissionService.create(createPermissionDto);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: number, @Body() updatePermissionDto: UpdatePermissionDto) {
+    return this.permissionService.update(id, updatePermissionDto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return this.permissionService.delete(id);
+  }
 }
--- src/permission/permission.service.ts
+++ src/permission/permission.service.ts
@@ -1,8 +1,9 @@
-// permission.service.ts
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Permission } from './permission.entity';
+import { CreatePermissionDto } from './dto/create-permission.dto';
+import { UpdatePermissionDto } from './dto/update-permission.dto';
 
 @Injectable()
 export class PermissionService {
@@ -11,5 +12,33 @@ export class PermissionService {
     private readonly permissionRepository: Repository<Permission>,
   ) {}
 
-  // Implement your permission-related operations here
+  async findAll(): Promise<Permission[]> {
+    return this.permissionRepository.find();
+  }
+
+  async findById(id: number): Promise<Permission> {
+    return this.permissionRepository.findOneBy({id: id});
+  }
+
+  async create(createPermissionDto: CreatePermissionDto): Promise<Permission> {
+    const permission = this.permissionRepository.create(createPermissionDto);
+    return this.permissionRepository.save(permission);
+  }
+
+  async update(id: number, updatePermissionDto: UpdatePermissionDto): Promise<Permission> {
+    const permission = await this.permissionRepository.findOneBy({id: id});
+    if (!permission) {
+      throw new Error('Permission not found');
+    }
+    Object.assign(permission, updatePermissionDto);
+    return this.permissionRepository.save(permission);
+  }
+
+  async delete(id: number): Promise<void> {
+    const permission = await this.permissionRepository.findOneBy({id: id});
+    if (!permission) {
+      throw new Error('Permission not found');
+    }
+    await this.permissionRepository.remove(permission);
+  }
 }
